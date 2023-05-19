b7be5218f599579e290537056b38fbeee5839535
TomtePain <123365386+TomtePain@users.noreply.github.com>
2023-05-19 12:36:53 +0200

    cleaner

--- js/script.js
+++ js/script.js
@@ -111,3 +111,16 @@ function showLegalEN() {
     document.getElementById('legalenglish').classList.remove('d-none');
     document.getElementById('legalgerman').classList.add('d-none');
 }
+
+/**
+ * If the login menu has the class 'd-none', remove it. Otherwise, add it
+ */
+function subMenu() {
+    let menu = document.getElementById('logMenue');
+    
+    if (menu.classList.contains('d-none')) {
+        menu.classList.remove('d-none');
+    } else {
+        menu.classList.add('d-none');
+    }
+}
